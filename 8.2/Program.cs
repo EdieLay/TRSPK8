c198d6dbf8393ee1ef027fd6e4fac84d84145ea9
EdieLay <98664652+EdieLay@users.noreply.github.com>
2022-10-24 20:11:17 +0300

    Добавьте файлы проекта.

---
+++ 8.2/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+GenericMethods<int> intMethods = new GenericMethods<int>();
+int intObj = intMethods.Sum(1, 2, 3, 4, 5);
+Console.WriteLine($"Сумма int от 1 до 5: {intObj}");
+intObj = intMethods.Max(1, 2, 3, 4, 5);
+Console.WriteLine($"Максимальный из int от 1 до 5: {intObj}");
+intObj = intMethods.Min(1, 2, 3, 4, 5);
+Console.WriteLine($"Минимальный из int от 1 до 5: {intObj}");
+int a = 5, b = 10;
+Console.WriteLine($"a = {a}, b = {b}");
+intMethods.Swap(ref a, ref b);
+Console.WriteLine($"После swap: a = {a}, b = {b}");
+intObj = intMethods.Avg(3, 6, 2, 7, 10);
+Console.WriteLine($"Среденее значение чисел 3, 6, 2, 7, 10: {intObj}");
+
+Console.WriteLine();
+
+GenericMethods<double> doubleMethods = new GenericMethods<double>();
+double doubleObj = doubleMethods.Sum(1, -2.31, 3.51, 4.1, 5.86);
+Console.WriteLine($"Сумма double 1, -2.31, 3.51, 4.1, 5.86: {doubleObj}");
+doubleObj = doubleMethods.Max(1, -2.31, 3.51, 4.1, 5.86);
+Console.WriteLine($"Максимальный из double 1, -2.31, 3.51, 4.1, 5.86: {doubleObj}");
+doubleObj = doubleMethods.Min(1, -2.31, 3.51, 4.1, 5.86);
+Console.WriteLine($"Минимальный из double 1, -2.31, 3.51, 4.1, 5.86: {doubleObj}");
+double c = 5.135, d = 10.753;
+Console.WriteLine($"c = {c}, d = {d}");
+doubleMethods.Swap(ref c, ref d);
+Console.WriteLine($"После swap: c = {c}, d = {d}");
+doubleObj = doubleMethods.Avg(3, 6, 2, 7, 10);
+Console.WriteLine($"Среденее значение чисел 3, 6, 2, 7, 10: {doubleObj}");
+
+Console.WriteLine();
+
+//GenericMethods<MyStruct> MyStructMethods = new GenericMethods<MyStruct>();
+//MyStruct my1 = new MyStruct();
+//MyStruct my2 = new MyStruct(10);
+//MyStruct myObj = MyStructMethods.Sum(my1, my2);
+//Console.WriteLine($"Сумма MyStruct 0 и 10: {myObj}");
+
+
+
+class GenericMethods<T> where T : struct
+{
+    public T Sum(params T[] list)
+    {
+        T sum = default(T);
+        for (int i = 0; i < list.Length; i++)
+            sum += (dynamic)list[i];
+        return sum;
+    }
+    public T Max(params T[] list)
+    {
+        T max = list[0];
+        for (int i = 1; i < list.Length; i++)
+            if ((dynamic)list[i] > max)
+                max = list[i];
+        return max;
+    }
+    public T Min(params T[] list)
+    {
+        T min = list[0];
+        for (int i = 1; i < list.Length; i++)
+            if ((dynamic)list[i] < min)
+                min = list[i];
+        return min;
+    }
+    public void Swap(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public T Avg(params T[] list)
+    {
+        T avg = default(T);
+        for (int i = 0; i < list.Length; i++)
+            avg += (dynamic)list[i];
+        avg /= (dynamic)list.Length;
+        return avg;
+    }
+}
+
+struct MyStruct
+{
+    public int a { get; set; }
+    public MyStruct()
+    {
+        a = 0;
+    }
+    public MyStruct(int a)
+    {
+        this.a = a;
+    }
+}
---
+++ 8.4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace TRSPK8._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] graph;
+            int[,] knownPairs;
+            int n, m;
+            using (StreamReader sr = new StreamReader("INPUT.TXT"))
+            {
+                int high, low;
+                string[] input = sr.ReadLine().Split();
+                n = int.Parse(input[0]);
+                m = int.Parse(input[1]);
+                graph = new int[n, n];
+                knownPairs = new int[2, m];
+                for (int i = 0; i < m; i++)
+                {
+                    input = sr.ReadLine().Split();
+                    high = int.Parse(input[0]);
+                    low = int.Parse(input[1]);
+                    graph[high - 1, low - 1] = 1;
+                    knownPairs[0, i] = high - 1;
+                    knownPairs[1, i] = low - 1;
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("OUTPUT.TXT"))
+            {
+                for (int i = 0; i < knownPairs.GetLength(1); i++)
+                {
+                    if (graph[knownPairs[0, i], knownPairs[1, i]] == 1)
+                        if (IsCyclic(ref graph, knownPairs[0, i], knownPairs[1, i]))
+                        {
+                            sw.WriteLine("No");
+                            return;
+                        }
+                }
+                sw.WriteLine("Yes");
+            }
+            return;
+        }
+        static bool IsCyclic(ref int[,] graph, int high, int low)
+        {
+            graph[high, low]++;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[low, i] == 2)
+                    return true;
+                if (graph[low, i] == 1)
+                    if (IsCyclic(ref graph, low, i))
+                        return true;
+            }
+            graph[high, low]++;
+            return false;
+        }
+    }
+}
